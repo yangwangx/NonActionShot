586fdd6ac546ae3281053463336d779fceeb4e5a
Yang Wang <yangwangx@gmail.com>
2017-02-28 15:41:07 -0500

    Fix a bug

--- dependences/C/get_code.sh
+++ dependences/C/get_code.sh
@@ -1,6 +1,6 @@
 # code for iDTD
 wget http://vision.cs.stonybrook.edu/~yangwang/home/doc/projects/NonAction/data/improved_trajectory.tar
-tar -x improved_trajectory.tar
+tar -xf improved_trajectory.tar
 rm improved_trajectory.tar
 
 # code for optical flow
